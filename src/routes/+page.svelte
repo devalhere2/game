acb03651d1dbbbeecf476dcb9c9f6f1c00011939
deval <deval2299@gmail.com>
2024-04-10 12:00:07 +0530

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div style="display: flex;">
+    <h3 style="flex: 1;">Welcome to game app</h3>
+    <h3>
+        <a href="/login">Log In</a>
+    </h3>
+</div>
+<ul>
+    <a href="/uno"><li>Uno</li></a>
+    <a href="/unonm"><li>Uno No mercy</li></a>
+    <a href="/chess"><li>Chess</li></a>
+    <a href="/poker"><li>Poker</li></a>
+</ul>
